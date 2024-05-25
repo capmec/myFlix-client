d8dccdbd04155c7b932a7d145f10becb2e8ee2df
capmec <capmec@gmail.com>
2024-05-25 19:52:16 +0200

    add to favorites working (HAVE to change local api)

--- src/components/navigation-bar/navigation-bar.jsx
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 import { Navbar, Col, Container, NavDropdown, Row } from 'react-bootstrap'
 import { NavLink } from 'react-router-dom'
@@ -7,6 +8,32 @@ import { FaUserAstronaut } from 'react-icons/fa'
 import './navigation-bar.scss'
 
 export const NavigationBar = ({ user, onLoggedOut }) => {
+	const storedToken = localStorage.getItem('token')
+	const [username, setUsername] = useState([])
+	const [token, setToken] = useState(storedToken ? storedToken : null)
+
+	useEffect(() => {
+		if (!token) {
+			return
+		}
+		fetch('https://movie-api-o5p9.onrender.com/users', {
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
+		})
+			.then((response) => response.json())
+			.then((users) => {
+				const usersApi = users.map((user) => {
+					return {
+						id: user._id,
+						username: user.username,
+					}
+				})
+				localStorage.setItem('users', JSON.stringify(usersApi))
+				setUsername(usersApi)
+			})
+	}, [token])
+
 	return (
 		<Navbar className='navbar-custom'>
 			<Container>
@@ -18,11 +45,15 @@ export const NavigationBar = ({ user, onLoggedOut }) => {
 			<div className='options'>
 				<Col xs='auto'>
 					<Row>
-						<FaUserAstronaut size={36} />
+						<FaUserAstronaut size={32} />
 					</Row>
 					<Row>
-						<NavDropdown title='Username' id='basic-nav-dropdown'>
-							<NavDropdown.Item href='/profile'>Profile</NavDropdown.Item>
+						<NavDropdown title={user.username} id='basic-nav-dropdown'>
+							{/* when click go to the profile.userId */}
+							<NavDropdown.Item as={NavLink} to={`/users/${user._id}`}>
+								Profile
+							</NavDropdown.Item>
+
 							<NavDropdown.Item href='/login' onClick={onLoggedOut}>
 								Sign Out
 							</NavDropdown.Item>
